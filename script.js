73223c0f05e985aca726b0fff9c0cdc360d4b151
TraafNorT <traafforwork@gmail.com>
2020-12-21 11:25:52 +0300

    commit

---
+++ script.js
@@ -0,0 +1,275 @@
+var data = [
+{
+"id":"1",
+"class":"grid-item-big",
+"url":"/tire catalog",
+"image":"//f.nodacdn.net/381508",
+"title":"Шины"
+},
+{
+"id":"2",
+"class":"grid-item-small",
+"url":"/accessories catalog",
+"image":"//f.nodacdn.net/381521",
+"title":"Аксессуары"
+},
+{
+"id":"3",
+"class":"grid-item-vertical",
+"url":"/lamps catalog",
+"image":"//f.nodacdn.net/381510",
+"title":"Лампы"
+},
+{
+"id":"4",
+"class":"grid-item-small",
+"url":"/wipers catalog",
+"image":"//f.nodacdn.net/381511",
+"title":"Щётки"
+},
+{
+"id":"5",
+"class":"grid-item-small",
+"url":"/laximo",
+"image":"//f.nodacdn.net/381521",
+"title":"Оригинальные Каталоги"
+},
+{
+"id":"6",
+"class":"grid-item-small",
+"url":"/disks catalog",
+"image":"//f.nodacdn.net/381513",
+"title":"Диски"
+},
+{
+"id":"7",
+"class":"grid-item-small",
+"url":"/batteries catalog",
+"image":"//f.nodacdn.net/381514",
+"title":"Аккумуляторы"
+},
+{
+"id":"8",
+"class":"grid-item-small",
+"url":"/gear oils catalog",
+"image":"//f.nodacdn.net/381515",
+"title":"Трансмиссионные масла"
+},
+{
+"id":"9",
+"class":"grid-item-horizontal-long",
+"url":"/oils catalog",
+"image":"//f.nodacdn.net/381516",
+"title":"Масла"
+},
+{
+"id":"10",
+"class":"grid-item-vertical",
+"url":"/emergency waistcoats catalog",
+"image":"//f.nodacdn.net/381517",
+"title":"Жилеты светоотражающие"
+},
+{
+"id":"11",
+"class":"grid-item-small",
+"url":"/carbase",
+"image":"//f.nodacdn.net/381518",
+"title":"Запчасти для ТО"
+},
+{
+"id":"12",
+"class":"grid-item-small",
+"url":"/car freshners catalog",
+"image":"//f.nodacdn.net/381547",
+"title":"Ароматизаторы"
+},
+{
+"id":"13",
+"class":"grid-item-small",
+"url":"/washer liquids catalog",
+"image":"//f.nodacdn.net/381520",
+"title":"Жидкости стеклоомывателя"
+},
+{
+"id":"14",
+"class":"grid-item-small",
+"url":"/moto tires catalog",
+"image":"//f.nodacdn.net/381522",
+"title":"Шины мото"
+},
+{
+"id":"15",
+"class":"grid-item-horizontal-small",
+"url":"/ext cleaners catalog",
+"image":"//f.nodacdn.net/381523",
+"title":"Уход и очистка"
+},
+{
+"id":"16",
+"class":"grid-item-small",
+"url":"/brake fluids catalog",
+"image":"//f.nodacdn.net/381524",
+"title":"Жидкости тормозные"
+},
+{
+"id":"17",
+"class":"grid-item-small",
+"url":"/lug wrenches catalog",
+"image":"//f.nodacdn.net/381545",
+"title":"Ключи баллонные"
+},
+{
+"id":"18",
+"class":"grid-item-horizontal-small",
+"url":"/tool sets catalog",
+"image":"//f.nodacdn.net/381525",
+"title":"Наборы инструментов"
+},
+{
+"id":"19",
+"class":"grid-item-small",
+"url":"/oil additives catalog",
+"image":"//f.nodacdn.net/381526",
+"title":"Присадки в масла"
+},
+{
+"id":"20",
+"class":"grid-item-small",
+"url":"/fuel additives catalog",
+"image":"//f.nodacdn.net/381527",
+"title":"Присадки топливные"
+},
+{
+"id":"21",
+"class":"grid-item-small",
+"url":"/coolant catalog",
+"image":"//f.nodacdn.net/381528",
+"title":"Жидкости охлаждающие"
+},
+{
+"id":"22",
+"class":"grid-item-small",
+"url":"/steering wheel cover catalog",
+"image":"//f.nodacdn.net/381529",
+"title":"Оплётки на руль"
+},
+{
+"id":"23",
+"class":"grid-item-small",
+"url":"/poly v belts catalog",
+"image":"//f.nodacdn.net/381530",
+"title":"Ремни поликлиновые"
+},
+{
+"id":"24",
+"class":"grid-item-small",
+"url":"/light",
+"image":"//f.nodacdn.net/381531",
+"title":"Лампы по типам"
+},
+{
+"id":"25",
+"class":"grid-item-vertical",
+"url":"/antiseptics catalog",
+"image":"//f.nodacdn.net/381532",
+"title":"Антисептики"
+},
+{
+"id":"26",
+"class":"grid-item-small",
+"url":"/truck tires catalog",
+"image":"//f.nodacdn.net/381533",
+"title":"Шины грузовые"
+},
+{
+"id":"27",
+"class":"grid-item-small",
+"url":"/lubricants catalog",
+"image":"//f.nodacdn.net/381534",
+"title":"Смазки"
+},
+{
+"id":"28",
+"class":"grid-item-small",
+"url":"/brushes scrapers catalog",
+"image":"//f.nodacdn.net/381535",
+"title":"Очистка от снега, льда и воды"
+},
+{
+"id":"29",
+"class":"grid-item-small",
+"url":"/cooling system additives catalog",
+"image":"//f.nodacdn.net/381536",
+"title":"Присадки в систему охлаждения"
+},
+{
+"id":"30",
+"class":"grid-item-vertical",
+"url":"/scooter catalog",
+"image":"//f.nodacdn.net/381537",
+"title":"Самокаты"
+},
+{
+"id":"31",
+"class":"grid-item-small",
+"url":"/screwdrivers catalog",
+"image":"//f.nodacdn.net/381538",
+"title":"Отвёртки"
+},
+{
+"id":"32",
+"class":"grid-item-small",
+"url":"/industrial gear oils catalog",
+"image":"//f.nodacdn.net/381539",
+"title":"Масла редукторные"
+},
+{
+"id":"33",
+"class":"grid-item-small",
+"url":"/hydraulic oil catalog",
+"image":"//f.nodacdn.net/381540",
+"title":"Масла гидравлические"
+},
+{
+"id":"34",
+"class":"grid-item-small",
+"url":"/flushing oil catalog",
+"image":"//f.nodacdn.net/381541",
+"title":"Масла промывочные"
+},
+{
+"id":"35",
+"class":"grid-item-small",
+"url":"/sockets catalog",
+"image":"//f.nodacdn.net/381542",
+"title":"Головки торцевые"
+},
+{
+"id":"36",
+"class":"grid-item-horizontal-long",
+"url":"/bicycles catalog",
+"image":"//f.nodacdn.net/381543",
+"title":"Велосипеды"
+},
+{
+"id":"37",
+"class":"grid-item-horizontal-small",
+"url":"/compressors catalog",
+"image":"//f.nodacdn.net/381544",
+"title":"Компрессоры"
+},
+{
+"id":"38",
+"class":"grid-item-small",
+"url":"/adblue liquids catalog",
+"image":"//f.nodacdn.net/381546",
+"title":"Жидкости AdBlue"
+}
+]
+var mother = document.getElementById('mother');
+for (i = 0; i < data.length; i++) {
+  var child = document.createElement('div');
+  child.className += data[i]['class']
+  child.innerHTML = '<a href="' + data[i]['url'] + '" alt="'+ data[i]['title'] + '" title="'+ data[i]['title'] + '"><img src="http:' + data[i]['image'] + '"><span class="float-right">'+ data[i]['title'] + '</span></a>'
+  mother.append(child);
+}
